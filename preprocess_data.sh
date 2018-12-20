8650dc1e11ee067c9a17e046f3d9571d1fbdefec
Colin <colin.clarke@nibrt.ie>
2018-12-20 15:21:57 +0000

    initial

---
+++ preprocess_data.sh
@@ -0,0 +1,23 @@
+SAMPLEID=$1
+INDIR=$2
+OUTDIR=$3
+
+mkdir -p $OUTDIR
+
+# trim adapter using cut adapt
+cutadapt  -A AGATCGGAAGAGC  -a AGATCGGAAGAGC \
+          -o $OUTDIR/"$SAMPLEID"_1_sequence.cutadapt.txt.gz -p $OUTDIR/"$SAMPLEID"_2_sequence.cutadapt.txt.gz \
+          $INDIR/"$SAMPLEID"_1_sequence.txt.gz $INDIR/"$SAMPLEID"_2_sequence.txt.gz
+          
+mkdir -p $OUTDIR/paired $OUTDIR/unpaired
+
+trimmomatic-0.36.jar PE \
+                     -threads 32 \
+                     $OUTDIR/"$SAMPLEID"_1_sequence.cutadapt.txt.gz $OUTDIR/"$SAMPLEID"_2_sequence.cutadapt.txt.gz \
+                     $OUTDIR/unpaired/"$SAMPLEID"_1_sequence.txt.gz $OUTDIR/unpaired/"$SAMPLEID"_2_sequence.txt.gz \
+                     $OUTDIR/paired/"$SAMPLEID"_1_sequence.txt.gz $OUTDIR/paired/"$SAMPLEID"_2_sequence.txt.gz \
+                     SLIDINGWINDOW:4:20 \
+                     MINLEN:36 \
+                     -trimlog $OUTDIR/"$SAMPLEID".trimmomatic.log   
+                     
+# END
---
+++ run.sh
@@ -0,0 +1,3 @@
+#
+
+bash preprocess_data.sh total-rna-1 ../data/raw ../data/preprocessed 
