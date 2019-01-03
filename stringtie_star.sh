499c7376354b4a9514f9c34dc386e51c191d6531
Colin <colin.clarke@nibrt.ie>
2019-01-03 13:58:16 +0000

    delete

--- stringtie_star.sh
+++
@@ -1,9 +0,0 @@
-SAMPLEID=$1
-mapped_dir="mapped"
-gtf_file="reference_genome/ensembl_chok1.gtf"
-
-mkdir -p stringtie_output
-output_dir=stringtie_output
-stringtie -p 32 -G $gtf_file -o $output_dir/"$SAMPLEID".gtf $mapped_dir/"$SAMPLEID".bam
-
-# END
